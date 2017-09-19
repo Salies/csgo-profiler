b2b3fedaa04889ebf83e5fdbbeda9f81146f4d9b
Salies <danielserezane@outlook.com>
2017-09-18 21:37:58 -0300

    "use strict" - server ready

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require('express')
 var request = require('request');
 var pug = require('pug');
@@ -30,7 +32,7 @@ app.get('/:input', (req, res) => {
         function(callback) {
             let url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${key}&vanityurl=${raw}`;
             request(url, function(error, response, res) {
-                r = JSON.parse(res);
+                let r = JSON.parse(res);
                 if (r.response.success == 42 && isNaN(raw) === false && raw.length == 17) {
                     //when the value is a steam64 id
                     let inp = raw;
@@ -70,7 +72,7 @@ app.get('/:input', (req, res) => {
             });
         }
     ], function(err, results) {
-        input = results[0];
+        var input = results[0];
         var summary = results[1];
         let ban_url = `https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=${key}&steamids=${input}`,
             cs_url = `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=${key}&steamid=${input}`;
@@ -166,7 +168,7 @@ app.get('/:input', (req, res) => {
                             var maps = [];
 
                             //smart tricky part - loop for organizing info and not making a monster array (on thefirst ones)
-                            for (i = 0; i < p.length; i++) {
+                            for (let i = 0; i < p.length; i++) {
                                 if ((p[i].name).startsWith("total_kills_") && p[i].name !== "total_kills_headshot" && p[i].name !== "total_kills_enemy_weapon" && p[i].name !== "total_kills_enemy_blinded" && p[i].name !== "total_kills_knife_fight" && p[i].name !== "total_kills_against_zoomed_sniper") {
                                     let gn = eval("gun_names." + (p[i].name).replace("total_kills_", ""));
                                     guns.push({
@@ -251,11 +253,11 @@ app.get('/:input', (req, res) => {
             let status = statuses[summary.personastate];
             var countries={AF:"Afghanistan",AX:"Aland Islands",AL:"Albania",DZ:"Algeria",AS:"American Samoa",AD:"Andorra",AO:"Angola",AI:"Anguilla",AQ:"Antarctica",AG:"Antigua And Barbuda",AR:"Argentina",AM:"Armenia",AW:"Aruba",AU:"Australia",AT:"Austria",AZ:"Azerbaijan",BS:"Bahamas",BH:"Bahrain",BD:"Bangladesh",BB:"Barbados",BY:"Belarus",BE:"Belgium",BZ:"Belize",BJ:"Benin",BM:"Bermuda",BT:"Bhutan",BO:"Bolivia",BA:"Bosnia And Herzegovina",BW:"Botswana",BV:"Bouvet Island",BR:"Brazil",IO:"British Indian Ocean Territory",BN:"Brunei Darussalam",BG:"Bulgaria",BF:"Burkina Faso",BI:"Burundi",KH:"Cambodia",CM:"Cameroon",CA:"Canada",CV:"Cape Verde",KY:"Cayman Islands",CF:"Central African Republic",TD:"Chad",CL:"Chile",CN:"China",CX:"Christmas Island",CC:"Cocos (Keeling) Islands",CO:"Colombia",KM:"Comoros",CG:"Congo",CD:"Congo, Democratic Republic",CK:"Cook Islands",CR:"Costa Rica",CI:"Cote D'Ivoire",HR:"Croatia",CU:"Cuba",CY:"Cyprus",CZ:"Czech Republic",DK:"Denmark",DJ:"Djibouti",DM:"Dominica",DO:"Dominican Republic",EC:"Ecuador",EG:"Egypt",SV:"El Salvador",GQ:"Equatorial Guinea",ER:"Eritrea",EE:"Estonia",ET:"Ethiopia",FK:"Falkland Islands (Malvinas)",FO:"Faroe Islands",FJ:"Fiji",FI:"Finland",FR:"France",GF:"French Guiana",PF:"French Polynesia",TF:"French Southern Territories",GA:"Gabon",GM:"Gambia",GE:"Georgia",DE:"Germany",GH:"Ghana",GI:"Gibraltar",GR:"Greece",GL:"Greenland",GD:"Grenada",GP:"Guadeloupe",GU:"Guam",GT:"Guatemala",GG:"Guernsey",GN:"Guinea",GW:"Guinea-Bissau",GY:"Guyana",HT:"Haiti",HM:"Heard Island & Mcdonald Islands",VA:"Holy See (Vatican City State)",HN:"Honduras",HK:"Hong Kong",HU:"Hungary",IS:"Iceland",IN:"India",ID:"Indonesia",IR:"Iran, Islamic Republic Of",IQ:"Iraq",IE:"Ireland",IM:"Isle Of Man",IL:"Israel",IT:"Italy",JM:"Jamaica",JP:"Japan",JE:"Jersey",JO:"Jordan",KZ:"Kazakhstan",KE:"Kenya",KI:"Kiribati",KR:"Korea",KW:"Kuwait",KG:"Kyrgyzstan",LA:"Lao People's Democratic Republic",LV:"Latvia",LB:"Lebanon",LS:"Lesotho",LR:"Liberia",LY:"Libyan Arab Jamahiriya",LI:"Liechtenstein",LT:"Lithuania",LU:"Luxembourg",MO:"Macao",MK:"Macedonia",MG:"Madagascar",MW:"Malawi",MY:"Malaysia",MV:"Maldives",ML:"Mali",MT:"Malta",MH:"Marshall Islands",MQ:"Martinique",MR:"Mauritania",MU:"Mauritius",YT:"Mayotte",MX:"Mexico",FM:"Micronesia, Federated States Of",MD:"Moldova",MC:"Monaco",MN:"Mongolia",ME:"Montenegro",MS:"Montserrat",MA:"Morocco",MZ:"Mozambique",MM:"Myanmar",NA:"Namibia",NR:"Nauru",NP:"Nepal",NL:"Netherlands",AN:"Netherlands Antilles",NC:"New Caledonia",NZ:"New Zealand",NI:"Nicaragua",NE:"Niger",NG:"Nigeria",NU:"Niue",NF:"Norfolk Island",MP:"Northern Mariana Islands",NO:"Norway",OM:"Oman",PK:"Pakistan",PW:"Palau",PS:"Palestinian Territory, Occupied",PA:"Panama",PG:"Papua New Guinea",PY:"Paraguay",PE:"Peru",PH:"Philippines",PN:"Pitcairn",PL:"Poland",PT:"Portugal",PR:"Puerto Rico",QA:"Qatar",RE:"Reunion",RO:"Romania",RU:"Russian Federation",RW:"Rwanda",BL:"Saint Barthelemy",SH:"Saint Helena",KN:"Saint Kitts And Nevis",LC:"Saint Lucia",MF:"Saint Martin",PM:"Saint Pierre And Miquelon",VC:"Saint Vincent And Grenadines",WS:"Samoa",SM:"San Marino",ST:"Sao Tome And Principe",SA:"Saudi Arabia",SN:"Senegal",RS:"Serbia",SC:"Seychelles",SL:"Sierra Leone",SG:"Singapore",SK:"Slovakia",SI:"Slovenia",SB:"Solomon Islands",SO:"Somalia",ZA:"South Africa",GS:"South Georgia And Sandwich Isl.",ES:"Spain",LK:"Sri Lanka",SD:"Sudan",SR:"Suriname",SJ:"Svalbard And Jan Mayen",SZ:"Swaziland",SE:"Sweden",CH:"Switzerland",SY:"Syrian Arab Republic",TW:"Taiwan",TJ:"Tajikistan",TZ:"Tanzania",TH:"Thailand",TL:"Timor-Leste",TG:"Togo",TK:"Tokelau",TO:"Tonga",TT:"Trinidad And Tobago",TN:"Tunisia",TR:"Turkey",TM:"Turkmenistan",TC:"Turks And Caicos Islands",TV:"Tuvalu",UG:"Uganda",UA:"Ukraine",AE:"United Arab Emirates",GB:"United Kingdom",US:"United States",UM:"United States Outlying Islands",UY:"Uruguay",UZ:"Uzbekistan",VU:"Vanuatu",VE:"Venezuela",VN:"Viet Nam",VG:"Virgin Islands, British",VI:"Virgin Islands, U.S.",WF:"Wallis And Futuna",EH:"Western Sahara",YE:"Yemen",ZM:"Zambia",ZW:"Zimbabwe"};            
             var g = "";
-            for (i = 0; i < cstats.top_guns.length; i++) {
+            for (let i = 0; i < cstats.top_guns.length; i++) {
                 g += String("<span>" + (i + 1) + ". <span style='color: #66c0f4;'>" + cstats.top_guns[i].name + " </span>(" + cstats.top_guns[i].kills + "</span> kills)</span><br>");
             }
             var m = "";
-            for (i = 0; i < cstats.top_maps.length; i++) {
+            for (let i = 0; i < cstats.top_maps.length; i++) {
                 m += String("<span>" + (i + 1) + ". <span style='color: #66c0f4;'>" + cstats.top_maps[i].name + " </span>(" + cstats.top_maps[i].rounds_won + "</span> rounds won)</span><br>");
             }
 
@@ -305,4 +307,4 @@ app.get('/:input', (req, res) => {
             res.render('profile', render);
         });
     });
-}).listen(8000); //or whatever port or server
+}).listen(80); //or whatever port or server
